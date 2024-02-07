c0e9f5ccc5b36c6f91f343b14394768ec7052be5
95tuanle <tlna95@icloud.com>
2024-02-07 17:34:51 -0500

    test

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 import * as morgan from 'morgan';
 
 async function bootstrap() {
+  console.log(
+    process.env['MONGODB_URI']
+      .replace('<password>', process.env.MONGODB_PASSWORD)
+      .replace('<database>', process.env.MONGODB_DATABASE),
+  );
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
   await app.listen(3000);
