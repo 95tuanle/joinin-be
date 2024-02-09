cc6fdde180adf42f7a4dde7bd6b33a6c41cd5a31
95tuanle <tlna95@icloud.com>
2024-02-08 20:13:39 -0500

    checkpoint

---
+++ src/event/event.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateEventDto } from 'src/dto/create-event.dto';
+import { UpdateEventDto } from 'src/dto/update-event.dto';
+import { Event } from 'src/schemas/event.schema';
+
+@Injectable()
+export class EventService {
+  constructor(@InjectModel(Event.name) private eventModel: Model<Event>) {}
+
+  async createEvent(createEventDto: CreateEventDto) {
+    const newEvent = new this.eventModel(createEventDto);
+    return newEvent.save();
+  }
+
+  getEvents() {
+    return this.eventModel.find();
+  }
+
+  getEventById(id: string) {
+    return this.eventModel.findById(id);
+  }
+
+  updateEvent(id: string, updateEventDto: UpdateEventDto) {
+    return this.eventModel.findByIdAndUpdate(id, updateEventDto, { new: true });
+  }
+
+  deleteEvent(id: string) {
+    return this.eventModel.findByIdAndDelete(id);
+  }
+}
--- src/schemas/event.schema.ts
+++ src/schemas/event.schema.ts
@@ -1,22 +1,21 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
-@Schema({timestamps: true})
+@Schema({ timestamps: true })
 export class Event {
+  @Prop({ required: true })
+  eventName: string;
 
-    @Prop({ required: true })
-    eventName: string;
+  @Prop({ required: true })
+  eventDesc: string;
 
-    @Prop({ required: true })
-    eventDesc: string;
+  @Prop({ required: true })
+  eventVenue: string;
 
-    @Prop({ required: true })
-    eventVenue: string;
+  @Prop({ required: true })
+  eventDateTime: Date;
 
-    @Prop({ required: true })
-    eventDateTime: Date;
-
-    @Prop({ required: true, default: true })
-    validEvent: boolean;
+  @Prop({ required: true, default: true })
+  validEvent: boolean;
 }
 
 export const EventSchema = SchemaFactory.createForClass(Event);
